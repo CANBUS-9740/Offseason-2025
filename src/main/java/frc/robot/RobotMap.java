1e991e3b17c60f5fbad7d0b6c9cf78b241c576cb
LukeFN8 <gil.tanny@gmail.com>
2025-09-09 18:17:41 +0300

    added a ratio? I guess, don't really know how to incorperate a 3:1 ratio into the code other than writing 3 and you just devide the spin of the wheels by it

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,6 +3,7 @@ package frc.robot;
 public class RobotMap {
     public static final int SHOOTER_MOTOR=0;
     public static final int IR_PROXIMITY_SENSOR=1;
+    public static final int GEAR_RATIO=3;
     private RobotMap() {
 
     }
