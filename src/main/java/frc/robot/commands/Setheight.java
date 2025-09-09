65942dd44991b3201902811d6c57bb402dc7aed0
LukeFN8 <gil.tanny@gmail.com>
2025-09-09 20:57:53 +0300

    made a very bad elevator thing

--- src/main/java/frc/robot/commands/Setheight.java
+++ src/main/java/frc/robot/commands/Setheight.java
@@ -1,9 +1,48 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotMap;
+import frc.robot.subsystems.Elevator;
 
 public class Setheight extends Command {
     private double height;
-    private final 
-    public Setheight() {}
+    private final Elevator elevator;
+    private double NR;//Number of Revolutions
+    private double CH;//Current Height
+    private double pow;
+
+    public Setheight(Elevator elevator, double height) {
+        this.elevator = elevator;
+        this.height = height;
+    }
+
+    @Override
+    public void initialize() {
+        NR = this.height / RobotMap.CIRCUMFERENCE;
+        CH = RobotMap.CIRCUMFERENCE * elevator.getmotorPosition();
+        if (height < CH) {
+            elevator.move(0.3);
+        } else if (height > NR) {
+            elevator.move(-0.3);
+        } else {
+            elevator.stop();
+        }
+    }
+
+    @Override
+    public void execute() {
+        if (CH > this.height + 10) {
+            initialize();
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return CH > this.height - 10 && CH < this.height + 10;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevator.stop();
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,42 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
+
+public class Elevator extends SubsystemBase {
+    private final SparkMax leftMotor;
+    private final SparkMax rightMotor;
+    private final DigitalInput Limitswitch;
+    private RelativeEncoder encoder;
+    private boolean ispos=false;
+
+    public Elevator() {
+        leftMotor = new SparkMax(RobotMap.LEFTM, SparkLowLevel.MotorType.kBrushless);
+        rightMotor = new SparkMax(RobotMap.RIGHTM, SparkLowLevel.MotorType.kBrushless);
+        Limitswitch = new DigitalInput(RobotMap.LIMITER);
+        encoder = leftMotor.getEncoder();
+
+    }
+    public void move(double pow) {
+        leftMotor.set(pow);
+        rightMotor.set(-pow);
+    }
+    public void stop() {
+        leftMotor.stopMotor();
+        rightMotor.stopMotor();
+    }
+    public double getmotorPosition() {
+        return encoder.getPosition();
+    }
+    public void inposition(boolean isit) {
+        ispos = isit;
+
+    }
+    public boolean getispos() {
+        return ispos;
+    }
+}
