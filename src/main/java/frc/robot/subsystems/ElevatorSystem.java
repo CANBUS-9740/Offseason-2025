9da0f7a60e8e9cd9f3bbb9c3aa6c78beed5cd973
talya <talya.duani123@gmail.com>
2025-09-29 21:29:06 +0300

    made the elevator use absolute encoder

--- src/main/java/frc/robot/subsystems/ElevatorSystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSystem.java
@@ -1,26 +1,26 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
 public class ElevatorSystem extends SubsystemBase {
     private final SparkMax motorMaster;
     private final SparkMax motorFollow;
-    private final RelativeEncoder encoder;
-    private double targetHeight;
+    private final AbsoluteEncoder encoder;
 
 
     public ElevatorSystem() {
         motorMaster = new SparkMax(RobotMap.ELEVATOR_MASTER_MOTOR, SparkLowLevel.MotorType.kBrushless);
         motorFollow = new SparkMax(RobotMap.ELEVATOR_FOLLOW_MOTOR, SparkLowLevel.MotorType.kBrushless);
-        encoder = motorMaster.getEncoder();
+        encoder = motorMaster.getAbsoluteEncoder();
 
         SparkMaxConfig configMaster = new SparkMaxConfig();
         configMaster.closedLoop
@@ -28,7 +28,7 @@ public class ElevatorSystem extends SubsystemBase {
                 .i(RobotMap.ELEVATOR_I)
                 .d(RobotMap.ELEVATOR_D)
                 .velocityFF(RobotMap.ELEVATOR_FF);
-        configMaster.closedLoop.feedbackSensor(ClosedLoopConfig.FeedbackSensor.kPrimaryEncoder);
+        configMaster.closedLoop.feedbackSensor(ClosedLoopConfig.FeedbackSensor.kAbsoluteEncoder);
         configMaster.encoder.positionConversionFactor(1 / RobotMap.GEAR_RATIO_ELEVATOR);
         motorMaster.configure(configMaster, SparkBase.ResetMode.kResetSafeParameters , SparkBase.PersistMode.kNoPersistParameters);
 
@@ -41,16 +41,21 @@ public class ElevatorSystem extends SubsystemBase {
         motorMaster.stopMotor();
     }
 
-    public double getHeight(){
-        return encoder.getPosition() * RobotMap.CIRCUMFERNCE_MM;
+    public double getHeightM(){
+        return encoder.getPosition() * RobotMap.CIRCUMFERENCE_M / 10;
     }
 
-    public void moveToHeight(double targetHeight){
-        motorMaster.getClosedLoopController().setReference(targetHeight / RobotMap.CIRCUMFERNCE_MM, SparkBase.ControlType.kPosition);
+    public void moveToHeight(double targetHeightM){
+        motorMaster.getClosedLoopController().setReference(targetHeightM / RobotMap.CIRCUMFERENCE_M, SparkBase.ControlType.kPosition);
     }
 
-    public boolean isAtHeight(double targetHeight){
-        return MathUtil.isNear(targetHeight,getHeight(), 0.1);
+    public boolean isAtHeight(double targetHeightM){
+        return MathUtil.isNear(targetHeightM,getHeightM(), 0.1);
     }
 
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("ElevatorHeightM", getHeightM());
+        SmartDashboard.putNumber("encoderPosition", encoder.getPosition());
+    }
 }
